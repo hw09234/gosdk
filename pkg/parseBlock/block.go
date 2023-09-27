ee539faa1ef9356fce0e6673ad6cb31e1e424e21
hw09234 <huangwei@peerfintech.cn>
2023-09-27 15:23:07 +0800

    feat: 添加关于kafka配置解析的protobuf.

--- pkg/parseBlock/block.go
+++ pkg/parseBlock/block.go
@@ -10,17 +10,18 @@ import (
 	"fmt"
 	"time"
 
+	ab "github.com/hw09234/gosdk/pkg/protos"
+
 	"github.com/golang/protobuf/proto"
+	"github.com/hw09234/gm-crypto/x509"
 	cb "github.com/hyperledger/fabric-protos-go/common"
 	"github.com/hyperledger/fabric-protos-go/ledger/rwset"
 	"github.com/hyperledger/fabric-protos-go/ledger/rwset/kvrwset"
 	pbmsp "github.com/hyperledger/fabric-protos-go/msp"
-//	ab "github.com/hyperledger/fabric-protos-go/orderer"
 	"github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/hyperledger/fabric-protos-go/peer/lifecycle"
 	"github.com/hyperledger/fabric/common/flogging"
 	utils "github.com/hyperledger/fabric/protoutil"
-	"github.com/hw09234/gm-crypto/x509"
 	"github.com/pkg/errors"
 )
 
@@ -144,18 +145,18 @@ func getValueFromBlockMetadata(block *cb.Block, index cb.BlockMetadataIndex) []b
 		b := make([]byte, 8)
 		binary.LittleEndian.PutUint64(b, lastConfig.Index)
 		return b
-	//} else if index == cb.BlockMetadataIndex_ORDERER {
-	//	if err := proto.Unmarshal(block.Metadata.Metadata[index], valueMetadata); err != nil {
-	//		return nil
-	//	}
-	//
-	//	kafkaMetadata := &ab.KafkaMetadata{}
-	//	if err := proto.Unmarshal(valueMetadata.Value, kafkaMetadata); err != nil {
-	//		return nil
-	//	}
-	//	b := make([]byte, 8)
-	//	binary.LittleEndian.PutUint64(b, uint64(kafkaMetadata.LastOffsetPersisted))
-	//	return b
+	} else if index == cb.BlockMetadataIndex_ORDERER {
+		if err := proto.Unmarshal(block.Metadata.Metadata[index], valueMetadata); err != nil {
+			return nil
+		}
+
+		kafkaMetadata := &ab.KafkaMetadata{}
+		if err := proto.Unmarshal(valueMetadata.Value, kafkaMetadata); err != nil {
+			return nil
+		}
+		b := make([]byte, 8)
+		binary.LittleEndian.PutUint64(b, uint64(kafkaMetadata.LastOffsetPersisted))
+		return b
 	} else if index == cb.BlockMetadataIndex_TRANSACTIONS_FILTER {
 		return block.Metadata.Metadata[index]
 	}
